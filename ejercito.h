2a189fb95e601ac7af699794ed312a08b775cbcb
Juan <juan.coronado@correounivalle.edu.co>
2020-03-24 09:26:04 -0500

    Archivos base

---
+++ ejercito.h
@@ -0,0 +1,31 @@
+#ifndef _EJERCITO_H_
+#define _EJERCITO_H_
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <fstream>
+#include <math.h>
+
+using std::string;
+using namespace std;
+
+class Ejercito {
+
+public:
+	Ejercito();
+  ~Ejercito();
+
+protected:
+
+private:
+  int NumIntegrantes;
+  bool Tipo;
+
+};
+
+#endif 
---
+++ tablero.h
@@ -0,0 +1,32 @@
+#ifndef _TABLERO_H_
+#define _TABLERO_H_
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include<stack>
+
+using std::string;
+using namespace std;
+
+class Tablero {
+
+public:
+	Tablero();
+  ~Tablero();
+
+protected:
+
+private:
+  int DimensionX;
+  int DimensionY;
+
+};
+
+#endif 
